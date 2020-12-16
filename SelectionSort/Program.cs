74a35521a19e81a5d9fd0094139b6561660786ca
ThatOneDude2309 <73247641+ThatOneDude2309@users.noreply.github.com>
2020-12-16 18:29:45 +0300

    Add files via upload

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[9] { 29, 72, 98, 13, 87, 66, 52, 51, 36 };
+
+            int temp, min;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
