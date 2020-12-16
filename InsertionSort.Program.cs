f025cf9bdeb2150f5338fc33c014ec48e7a5b9c0
ThatOneDude2309 <73247641+ThatOneDude2309@users.noreply.github.com>
2020-12-16 18:28:10 +0300

    Add files via upload

---
+++ InsertionSort.Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Insertion_sort
+{
+    class Program
+    {
+        static int[] performInsertionSort(int[] inputarray)
+        {
+            for (int i = 0; i < inputarray.Length - 1; i++)
+            {
+                int j = i + 1;
+
+                while (j > 0)
+                {
+                    if (inputarray[j - 1] > inputarray[j])
+                    {
+                        int temp = inputarray[j - 1];
+                        inputarray[j - 1] = inputarray[j];
+                        inputarray[j] = temp;
+
+                    }
+                    j--;
+                }
+            }
+            return inputarray;
+        }
+    }
+}
+    
