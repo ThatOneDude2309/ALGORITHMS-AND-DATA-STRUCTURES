0c59c3753da0a1ecd0c076c08aaa57bb8908999d
ThatOneDude2309 <73247641+ThatOneDude2309@users.noreply.github.com>
2020-12-25 12:52:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tower = new int[45];
+            Random rnd = new Random();
+
+            for (int i = 0; i < tower.Length; i++)
+                tower[i] = rnd.Next(3, 9);
+
+            int j = 0;
+            int sum = 0;
+
+            do
+            {
+                sum += tower[j];
+                j++;
+            } while (j < tower.Length);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
